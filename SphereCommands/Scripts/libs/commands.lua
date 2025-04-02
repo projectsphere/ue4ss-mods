e0f1bddf7be0c9a8879b3e640b21279ecaa266f0
Kozejin <2613841+dkoz@users.noreply.github.com>
2025-04-02 15:02:30 -0400

    Code Refactoring, Code Logic Changes

---
+++ SphereCommands/Scripts/libs/commands.lua
@@ -0,0 +1,318 @@
+local PalUtilities = StaticFindObject("/Script/Pal.Default__PalUtility")
+local logMgr = FindFirstOf("BP_PalLogManager_C")
+local FGuid = require("util/fguid")
+
+local godmodePlayers = {}
+local noclipPlayers = {}
+local flyModePlayers = {}
+
+local commands = {}
+
+function commands.sendSystemAnnounce(PalPlayerController, Message)
+	ExecuteWithDelay(100, function()
+		local palUtility = StaticFindObject("/Script/Pal.Default__PalUtility")
+		local world = FindFirstOf("World")
+		local playerState = PalPlayerController.PlayerState
+		local playerUid = FGuid.translate(playerState.PlayerUId)
+		palUtility:SendSystemToPlayerChat(world, Message, playerUid)
+	end)
+end
+
+function commands.sendPersonalLog(playerController, msg)
+    if playerController and playerController:IsValid() and logMgr then
+        playerController:SendLog_ToClient(1, FText(msg), {})
+    end
+end
+
+function commands.IsServerSide()
+    return PalUtilities and PalUtilities:IsValid() and PalUtilities:IsDedicatedServer(PalUtilities)
+end
+
+function commands.spawnItem(playerState, item)
+    local quantity = 1
+    if string.find(item, ":") then
+        item, quantity = string.match(item, "(.*):(.*)")
+    end
+
+    local inventory = playerState:GetInventoryData()
+    if commands.IsServerSide() then
+        inventory:AddItem_ServerInternal(FName(item), quantity, false)
+    else
+        inventory:RequestAddItem(FName(item), quantity, false)
+    end
+end
+
+function commands.giveExperience(playerState, quantity)
+    local PlayerController = playerState:GetPlayerController()
+    local PlayerCharacter = PlayerController and PlayerController.Pawn
+    if not PlayerCharacter or not PlayerCharacter:IsValid() then return end
+
+    local WorldContext = PlayerCharacter:GetWorld()
+    if not WorldContext or not WorldContext:IsValid() then return end
+
+    local Location = PlayerCharacter:K2_GetActorLocation()
+    local ExpRadius = 1000.0
+
+    local PalUtility = StaticFindObject("/Script/Pal.Default__PalUtility")
+    if not PalUtility or not PalUtility:IsValid() then return end
+
+    PalUtility:GiveExpToAroundPlayerCharacter(WorldContext, Location, ExpRadius, quantity, true)
+end
+
+function commands.IsPlayerAdmin(playerState)
+    return playerState and playerState:IsValid() and playerState:GetPlayerController().bAdmin
+end
+
+function commands.BroadcastServerMessage(message)
+    local gameStateInstance = FindFirstOf("PalGameStateInGame")
+    if gameStateInstance and gameStateInstance:IsValid() then
+        gameStateInstance:BroadcastServerNotice(message)
+    end
+end
+
+function commands.IsGodmodeEnabled(playerId)
+    return godmodePlayers[playerId] == true
+end
+
+function commands.toggleGodmode(playerState, arg)
+    local playerId = playerState:GetPlayerId()
+    if not playerId then return end
+    local PlayerController = playerState:GetPlayerController()
+
+    if arg == "enable" then
+        godmodePlayers[playerId] = true
+        commands.sendSystemAnnounce(PlayerController, "Godmode enabled.")
+    elseif arg == "disable" then
+        godmodePlayers[playerId] = nil
+        commands.sendSystemAnnounce(PlayerController, "Godmode disabled.")
+    else
+        commands.sendSystemAnnounce(PlayerController, "Usage: !godmode enable | disable")
+    end
+end
+
+function commands.toggleFlyMode(playerState, arg)
+    local playerId = playerState:GetPlayerId()
+    if not playerId then return end
+    local PlayerController = playerState:GetPlayerController()
+    if not PlayerController or not PlayerController:IsValid() then return end
+
+    if arg == "enable" then
+        if not flyModePlayers[playerId] then
+            flyModePlayers[playerId] = true
+            PlayerController:StartFlyToServer()
+            commands.sendSystemAnnounce(PlayerController, "Fly mode has been activated.")
+        else
+            commands.sendSystemAnnounce(PlayerController, "Fly mode is already enabled.")
+        end
+    elseif arg == "disable" then
+        if flyModePlayers[playerId] then
+            flyModePlayers[playerId] = nil
+            PlayerController:EndFlyToServer()
+            commands.sendSystemAnnounce(PlayerController, "Fly mode has been deactivated.")
+        else
+            commands.sendSystemAnnounce(PlayerController, "Fly mode is already disabled.")
+        end
+    else
+        commands.sendSystemAnnounce(PlayerController, "Usage: !fly enable | disable")
+    end
+end
+
+function commands.toggleNoclip(playerState, arg)
+    local playerId = playerState:GetPlayerId()
+    if not playerId then return end
+    local PlayerController = playerState:GetPlayerController()
+    if not PlayerController or not PlayerController:IsValid() then return end
+
+    local PlayerCharacter = PlayerController.Pawn
+    if not PlayerCharacter or not PlayerCharacter:IsValid() then return end
+
+    if arg == "enable" then
+        if not noclipPlayers[playerId] then
+            noclipPlayers[playerId] = true
+            PlayerCharacter:SetSpectatorMode(true)
+            commands.sendSystemAnnounce(PlayerController, "Entered noclip mode.")
+        else
+            commands.sendSystemAnnounce(PlayerController, "Noclip mode is already enabled.")
+        end
+    elseif arg == "disable" then
+        if noclipPlayers[playerId] then
+            noclipPlayers[playerId] = nil
+            PlayerCharacter:SetSpectatorMode(false)
+            commands.sendSystemAnnounce(PlayerController, "Left noclip mode.")
+        else
+            commands.sendSystemAnnounce(PlayerController, "Noclip mode is already disabled.")
+        end
+    else
+        commands.sendSystemAnnounce(PlayerController, "Usage: !noclip enable | disable")
+    end
+end
+
+function commands.handleGiveCommand(playerState, rest)
+	local senderController = playerState:GetPlayerController()
+	if not rest or rest == "" then
+		commands.sendSystemAnnounce(senderController, "Usage: !give <name> item[:amount] [item2[:amount] ...]")
+		return
+	end
+
+	local args = {}
+	for word in rest:gmatch("%S+") do
+		table.insert(args, word)
+	end
+
+	if #args < 2 then
+		commands.sendSystemAnnounce(senderController, "Usage: !give <name> item[:amount] [item2[:amount] ...]")
+		return
+	end
+
+	local targetName = args[1]
+	local palUtility = StaticFindObject("/Script/Pal.Default__PalUtility")
+	local world = FindFirstOf("World")
+	local playerList = palUtility:GetPlayerListDisplayMessages(world)
+	local targetController = nil
+
+	if playerList then
+		for i = 1, #playerList do
+			local info = playerList[i]:get():ToString()
+			local name = info:match("^(.-),")
+			if name == targetName then
+				local playerChar = palUtility:GetPlayerCharacterByPlayerIndex(world, i - 1)
+				if playerChar and playerChar:IsValid() then
+					targetController = playerChar:GetPalPlayerController()
+					break
+				end
+			end
+		end
+	end
+
+	if not targetController then
+		commands.sendSystemAnnounce(senderController, "Player not found.")
+		return
+	end
+
+	local targetState = targetController.PlayerState
+
+	for i = 2, #args do
+		local item = args[i]
+		local qty = 1
+		local name = item
+
+		if item:find(":") then
+			name, qty = string.match(item, "^(.*):(%d+)$")
+			qty = tonumber(qty) or 1
+		end
+
+		commands.spawnItem(targetState, item)
+		commands.sendSystemAnnounce(senderController, string.format("Gave %d x %s to %s", qty, name, targetName))
+	end
+end
+
+function commands.handleExpCommand(playerState, rest)
+	local senderController = playerState:GetPlayerController()
+	if not rest or rest == "" then
+		commands.sendSystemAnnounce(senderController, "Usage: !exp <name> <amount>")
+		return
+	end
+
+	local args = {}
+	for word in rest:gmatch("%S+") do
+		table.insert(args, word)
+	end
+
+	if #args < 2 then
+		commands.sendSystemAnnounce(senderController, "Usage: !exp <name> <amount>")
+		return
+	end
+
+	local targetName = args[1]
+	local amount = tonumber(args[2])
+	if not amount then
+		commands.sendSystemAnnounce(senderController, "Invalid amount.")
+		return
+	end
+
+	local palUtility = StaticFindObject("/Script/Pal.Default__PalUtility")
+	local world = FindFirstOf("World")
+	local playerList = palUtility:GetPlayerListDisplayMessages(world)
+	local targetController = nil
+
+	if playerList then
+		for i = 1, #playerList do
+			local info = playerList[i]:get():ToString()
+			local name = info:match("^(.-),")
+			if name == targetName then
+				local playerChar = palUtility:GetPlayerCharacterByPlayerIndex(world, i - 1)
+				if playerChar and playerChar:IsValid() then
+					targetController = playerChar:GetPalPlayerController()
+					break
+				end
+			end
+		end
+	end
+
+	if not targetController then
+		commands.sendSystemAnnounce(senderController, "Player not found.")
+		return
+	end
+
+	local targetState = targetController.PlayerState
+	commands.giveExperience(targetState, amount)
+	commands.sendSystemAnnounce(senderController, string.format("Granted %d EXP to %s", amount, targetName))
+end
+
+function commands.handlePersonalGive(playerState, rest)
+	local PlayerController = playerState:GetPlayerController()
+	if not rest or rest == "" then
+		commands.sendSystemAnnounce(PlayerController, "Usage: !give item[:amount] [item2[:amount] ...]")
+		return
+	end
+
+	for item in rest:gmatch("%S+") do
+		local name = item
+		local qty = 1
+		if string.find(item, ":") then
+			name, qty = string.match(item, "^(.*):(%d+)$")
+			qty = tonumber(qty) or 1
+		end
+		commands.spawnItem(playerState, item)
+		commands.sendSystemAnnounce(PlayerController, string.format("Spawned %d x %s", qty, name))
+	end
+end
+
+function commands.handlePersonalExp(playerState, rest)
+	local PlayerController = playerState:GetPlayerController()
+	local amount = tonumber(rest)
+	if not amount then
+		commands.sendSystemAnnounce(PlayerController, "Usage: !exp <amount>")
+		return
+	end
+	commands.giveExperience(playerState, amount)
+	commands.sendSystemAnnounce(PlayerController, string.format("Granted %d EXP to your party.", amount))
+end
+
+function commands.handleSpectate(playerState)
+    local PlayerController = playerState:GetPlayerController()
+    if PlayerController and PlayerController:IsValid() then
+        PlayerController:ClientBeginSpectate(true)
+        commands.sendSystemAnnounce(PlayerController, "Entered spectator mode.")
+    end
+end
+
+function commands.handleAnnounce(playerState, rest)
+    local PlayerController = playerState:GetPlayerController()
+    if not rest or rest == "" then
+        commands.sendSystemAnnounce(PlayerController, "Usage: !announce <message>")
+        return
+    end
+    commands.BroadcastServerMessage(rest)
+    commands.sendSystemAnnounce(PlayerController, "Announced server wide message.")
+end
+
+function commands.handleUnstuck(playerState)
+    local PlayerController = playerState:GetPlayerController()
+    if PlayerController and PlayerController:IsValid() then
+        PlayerController:TeleportToSafePoint_ToServer()
+        commands.sendSystemAnnounce(PlayerController, "You have been teleported back to base!")
+    end
+end
+
+return commands
---
+++ SphereCommands/Scripts/libs/logic.lua
@@ -0,0 +1,104 @@
+local commands = require("libs/commands")
+local logic = {}
+
+function logic.damageHook(_, damageInfo)
+    local result = damageInfo:get()
+    if not result then return end
+    local defender = result.Defender
+    if not defender or not defender:IsValid() then return end
+    local state = defender.PlayerState
+    if not state or not state:IsValid() then return end
+    local playerId = state:GetPlayerId()
+    if commands.IsGodmodeEnabled(playerId) then
+        result.Damage = 0
+    end
+end
+
+function logic.statusHook(self, statusName, statusValue)
+    local allPlayers = FindAllOf("PalPlayerController")
+    for _, controller in pairs(allPlayers) do
+        if controller and controller:IsValid() then
+            local state = controller.PlayerState
+            if state and state:IsValid() then
+                local playerId = state:GetPlayerId()
+                if playerId and commands.IsGodmodeEnabled(playerId) then
+                    self:set(nil)
+                    statusName:set(nil)
+                    statusValue:set(nil)
+                    return
+                end
+            end
+        end
+    end
+end
+
+function logic.chatHook(ctx, chat)
+    local text = chat:get().Message:ToString()
+    local playerState = ctx:get()
+    local PlayerController = playerState:GetPlayerController()
+    local cmd, rest = text:match("^!(%S+)%s*(.*)$")
+    if cmd then
+        cmd = cmd:lower()
+        if cmd == "spectate" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.handleSpectate(playerState)
+        elseif cmd == "announce" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.handleAnnounce(playerState, rest)
+        elseif cmd == "godmode" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.toggleGodmode(playerState, rest:lower())
+        elseif cmd == "fly" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.toggleFlyMode(playerState, rest:lower())
+        elseif cmd == "noclip" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.toggleNoclip(playerState, rest:lower())
+        elseif cmd == "give" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.handleGiveCommand(playerState, rest)
+        elseif cmd == "exp" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.handleExpCommand(playerState, rest)
+        elseif cmd == 'giveme' then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.handlePersonalGive(playerState, rest)
+        elseif cmd == "giveexp" then
+            if not commands.IsPlayerAdmin(playerState) then
+                commands.sendSystemAnnounce(PlayerController, "You do not have permission")
+                return
+            end
+            commands.handlePersonalExp(playerState, rest)
+        elseif cmd == "help" then
+            commands.sendSystemAnnounce(PlayerController, "Available Commands:\n!help - Show help\n!give item:amount\n!exp <amount>\n!fly enable/disable\n!noclip enable/disable\n!godmode enable/disable\n!spectate\n!announce <msg>\n!unstuck")
+        elseif cmd == "unstuck" then
+            commands.handleUnstuck(playerState)
+        end
+    end
+end
+
+return logic
